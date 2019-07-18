11bfb6d98890148122f6ddca32a2930dcc4936e0
Ross Perkel <rossperkel@gmail.com>
2019-07-18 17:54:07 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,34 @@
-# Add your code here
+require 'pry'
+
+class Dog
+    @@all = []
+    
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def name
+        @name
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.print_all
+        @@all.each do |dog|
+            puts dog.name
+        end
+    end
+
+    def self.clear_all
+        @@all = []
+    end
+end
+
+
+
+lewis = Dog.new('Lewis')
+binding.pry
+puts 'fin'
